e1aff4cfc1eb55a111598e2e8a9084b6b8f84ee4
JJoaoll <joaoduos@gmail.com>
2024-10-17 14:21:06 -0300

    preguica de mexer com esses conflitos desse maldito Prelude.

--- haskell/MyInt.hs
+++ haskell/MyInt.hs
@@ -24,11 +24,38 @@ minu (Int n m) = Int m n
 minus :: Int -> Int -> Int 
 x `minus` y = x `plus` minu y 
 
-abs :: Int -> Int
-abs x  = P.undefined 
+sgn :: Int -> Int  
+sgn (Int n m)  
+  | n P.< m     = Int O (S O) 
+  | n P.== m    = Int O O 
+  | P.otherwise = Int (S O) O
 
+isNeg :: Int -> P.Bool
+isNeg x = sgn x P.== Int (S O) O  
+ 
+isNotNeg :: Int -> P.Bool 
+isNotNeg = P.not . isNeg 
+
+isPos :: Int -> P.Bool 
+isPos x = sgn x P.== Int O (S O)
+
+isNotPos :: Int -> P.Bool 
+isNotPos = P.not P.(.) isPos
+
+haveShameSgn :: Int -> Int -> P.Bool 
+haveShameSgn x y = (isNotNeg x P.&& isNotNeg y) 
+                  P.|| (isNotPos x P.&& isNotPos y)
+
+abs :: Int -> Int
+abs x
+  | sgn x P.== Int (S O) O = minu x 
+  | P.otherwise                 = x 
+ 
 times :: Int -> Int -> Int 
-x `times` y = P.undefined 
+x `times` y
+  | haveShameSgn x y = P.undefined 
+  | P.otherwise      = P.undefined 
+              
 
 x = Int (S O) O
 y = Int (S P.$ S O) (S O) 
