9364c57307e75acefa0bb0be51e36c5ff325c7df
JJoaoll <joaoduos@gmail.com>
2024-11-06 20:30:58 -0300

    update

--- haskell/Hw's/ExArith.hs
+++ haskell/Hw's/ExArith.hs
@@ -6,6 +6,8 @@ data ArEx = Atom Integer
           | Neg ArEx
   deriving (Eq, Show)
 
+
+
 -- pretty printer
 pretty :: ArEx -> String
 pretty (Atom n) = show n
@@ -36,9 +38,18 @@ ex5 = (Neg ex1) `Times` (Neg ex4)
 
 -- eval evaluates an expression and returns its value
 eval :: ArEx -> Integer
-eval = undefined
+eval (Atom t)     = t 
+eval (Plus t t')  = eval t + eval t' 
+eval (Times t t') = eval t * eval t'
+eval (Neg t)      = negate (eval t)
+
 
 -- step should make only 1 step of calculation on a given ArEx
 step :: ArEx -> ArEx
-step = undefined
+step (Atom t)     = Atom t 
+step (Plus t t')  = Atom (eval t + eval t')
+step (Times t t') = Atom () 
+step (Neg t)      = Atom (negate (eval t))
+
+  
 
